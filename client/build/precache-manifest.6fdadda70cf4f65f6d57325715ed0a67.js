7cb75f38606ea3ecbec6d621c7f8971f1f59cb2f
LM Train <layconmm@msn.com>
2020-02-04 18:58:01 -0600

    Run npm run build

---
+++ client/build/precache-manifest.6fdadda70cf4f65f6d57325715ed0a67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8dbc6e4768700030eba5adc85fedb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29c3954351eb7e5ea1e5",
+    "url": "/static/css/main.e83d3a9e.chunk.css"
+  },
+  {
+    "revision": "853dcc4d7ae71bf06d8f",
+    "url": "/static/js/2.7e9a72b3.chunk.js"
+  },
+  {
+    "revision": "29c3954351eb7e5ea1e5",
+    "url": "/static/js/main.35a6d782.chunk.js"
+  },
+  {
+    "revision": "37facd21dbf1673402a7",
+    "url": "/static/js/runtime~main.ce49a5c8.js"
+  }
+]);
